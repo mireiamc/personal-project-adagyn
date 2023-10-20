cecab909776e6cbdd5d5cb9e4aad4a5b42eb3ea2
¨mireiamc¨ <mireia.martin.carmona@gmail.com>
2023-10-20 10:19:47 +0200

    adding folders to the project

---
+++ src/components/App.jsx
@@ -0,0 +1,18 @@
+import '../styles/App.scss';
+
+// Fichero src/components/App.jsx
+// import {useState} from 'react';
+
+const App = () => {
+  
+  // const [email, setEmail] = useState('');
+
+  return (
+    <div className='container'>
+      <h1 className='title'>Holita</h1>
+      <h2>Tu puedes con todo :)</h2>
+    </div>
+  );
+};
+
+export default App;

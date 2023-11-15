75b73f652d5642f438230abac7f5dc94e4efaea7
¨mireiamc¨ <mireia.martin.carmona@gmail.com>
2023-11-15 19:24:36 +0100

    retouching some styles and adding cycle info at home page

--- src/components/Landing/Info.jsx
+++ src/components/Landing/Info.jsx
@@ -7,19 +7,38 @@ const Info = () => {
       <h1 className='info__title'>AdaGyn</h1>
       <h2 className='info__subtitle'>Tu app de salud femenina de confianza</h2>
       <p className='info__text'>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione nisi
-        saepe, ea modi facilis eveniet cupiditate autem porro. Praesentium quod
-        quaerat quibusdam sapiente recusandae alias cumque, corrupti ea deserunt
-        quis? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Mollitia
-        corporis qui modi obcaecati provident fugiat dolor, incidunt eius
-        architecto? Minus sed perferendis voluptatem consequatur architecto.
-        Dolore laudantium fugit nostrum alias! Lorem ipsum dolor sit amet
-        consectetur, adipisicing elit. Fuga quam odio debitis nam exercitationem
-        aliquid quos cupiditate, dicta, dignissimos, eum eaque magnam? Assumenda
-        velit aperiam ut. Molestiae quis dicta vitae. Lorem ipsum dolor sit amet
-        consectetur adipisicing elit. Tempora et veritatis minus laudantium
-        officiis labore quae accusamus sapiente autem aperiam ducimus vel, velit
-        ex aspernatur suscipit voluptas, quas facilis iste.
+        ¡Bienvenida a AdaGyn! La salud femenina es un pilar fundamental para la
+        vida y con el estrés al que nos vemos sometidas a diario cada vez es más
+        común desarrollar algún desajuste hormonal que puede derivar en alguna
+        enfermedad. Además, las mujeres pasamos por diferentes fases a lo largo
+        de nuestras vidas que afectan directamente a nuestras hormonas.
+        <br /> <br />
+        Muchas de las que hayáis llegado hasta aquí, habréis vivido lo que es
+        acudir a una consulta médica ginecológica o endocrinológica y la
+        complejidad de llevar un seguimiento ordenado de toda la información
+        relacionada con esas visitas. AdaGyn llega para poder solucionar estos
+        problemas y que puedas sentirte más segura y preparada a la hora de
+        afrontar estas visitas médicas.
+        <br />
+        <br />
+        Aquí podrás encontrar un espacio donde registrar tus análisis y ver el
+        historial de todos tus registros para que, de un vistazo, puedas
+        comparar valores clave. Además podrás seleccionar algunos parámetros y
+        ver la evolución en una gráfica que te generará automáticamente el
+        sistema. Tendrás además un espacio para hacer un seguimiento de tu
+        medicación y suplementación: nombres de las pastillas, cantidad y
+        regularidad. También tendrás un espacio donde guardar tus próximas citas
+        médicas con todos los detalles para que no se te traspapele nada y una
+        pequeña zona donde tener tus notas sobre las recomendaciones que te hace
+        cada doctor.
+        <br />
+        <br />
+        Sabemos que hay millones de apps móviles para llevar el control de tu
+        ciclo menstrual muy completas, así que aquí simplemente encontrarás un
+        pequeño apartado para poder tener un resumen de tu ciclo: duración del
+        ciclo, fecha del último ciclo, duración media y tipo de sangrado.
+        Englobando así los aspectos clave de tu ciclo para poder tenerlos de un
+        vistazo.
       </p>
       <Link to='/inicia-sesion' className='button--black'>
         Iniciar sesión
--- src/components/Main/Home.jsx
+++ src/components/Main/Home.jsx
@@ -36,56 +36,85 @@ const Home = () => {
       </nav>
       <section className='big__container'>
         <section className='left__container'>
-          <img className='home__image' src={grafico} alt='grafico' />
+          <div>
+            <h2 className='home__title'>Mis hormonas</h2>
+            <img className='home__image' src={grafico} alt='grafico' />
+          </div>
+          <section className='analysis__container'>
+            <div>
+              <h2 className='analysis__title'>Últimos análisis</h2>
+              <h3 className='analysis__title--date'>30 de Noviembre</h3>
+            </div>
+            <ul className='analysis__list'>
+              <li className='analysis__item'>
+                TSH <span>2.5</span>
+                <span className='happyface'>&#9786;</span>
+              </li>
+              <li className='analysis__item'>
+                Insulina <span>28</span>
+                <span className='happyface'>&#9786;</span>
+              </li>
+              <li className='analysis__item'>
+                T4 Libre <span>6</span>
+                <span className='sadface'>&#9785;</span>
+              </li>
+              <li className='analysis__item'>
+                Glucosa <span>68</span>
+                <span className='happyface'>&#9786;</span>
+              </li>
+              <li className='analysis__item'>
+                Calcio <span>90</span>
+                <span className='sadface'>&#9785;</span>
+              </li>
+            </ul>
+          </section>
         </section>
+
         <section className='right__container'>
-          <h3 className='right__container--title'>Mis próximas citas</h3>
-          <div className='date__container'>
-            <p className='date__container--subtitle'>Dra. Mari Mer. Pérez</p>
-            <p>Centro Womer</p>
-            <p>Ginecología</p>
-            <p className='date__container--subtitle'>
-              Viernes, 24 de diciembre
-            </p>
+          <div>
+            <h3 className='right__container--title'>Mis próximas citas</h3>
+            <div className='date__container'>
+              <p className='date__container--subtitle'>Dra. Mari Mer. Pérez</p>
+              <p>Centro Womer</p>
+              <p>Ginecología</p>
+              <p className='date__container--subtitle'>
+                Viernes, 24 de diciembre
+              </p>
+            </div>
           </div>
-          <h3 className='right__container--title'>Mi medicación</h3>
-          <div className='medication__container'>
-            <p className='medication__container--subtitle'>Eutirox 175mg</p>
-            <p>1 comprimido al día</p>
-            <p className='medication__container--subtitle'>
-              Myoinositol Lambert's
-            </p>
-            <p>3 comprimidos al día</p>
+          <div>
+            <h3 className='right__container--title'>Mi medicación</h3>
+            <div className='medication__container'>
+              <p className='medication__container--subtitle'>Eutirox 175mg</p>
+              <p>1 comprimido al día</p>
+              <p className='medication__container--subtitle'>
+                Myoinositol Lambert's
+              </p>
+              <p>3 comprimidos al día</p>
+            </div>
+          </div>
+          <div>
+            <h3 className='right__container--title'>Mi ciclo</h3>
+            <div className='cycle__content'>
+              <p className='highlight__text'>
+                Último ciclo: <br />{' '}
+                <span className='plain__text'>20 de Noviembre</span>
+              </p>
+              <p className='highlight__text'>
+                Duración del último ciclo: <br />
+                <span className='plain__text'>6 días</span>
+              </p>
+              <p className='highlight__text'>
+                Tipo de sangrado: <br />
+                <span className='plain__text'>Normal</span>
+              </p>
+              <p className='highlight__text'>
+                Media de duración del ciclo: <br />
+                <span className='plain__text'>35 días</span>
+              </p>
+            </div>
           </div>
         </section>
-      </section>
-      <section className='analysis__container'>
-        <div>
-          <h2 className='analysis__title'>Últimos análisis</h2>
-          <h3 className='analysis__title--date'>30 de Noviembre</h3>
-        </div>
-        <ul className='analysis__list'>
-          <li className='analysis__item'>
-            TSH <span>2.5</span>
-            <span className='happyface'>&#9786;</span>
-          </li>
-          <li className='analysis__item'>
-            Insulina <span>28</span>
-            <span className='happyface'>&#9786;</span>
-          </li>
-          <li className='analysis__item'>
-            T4 Libre <span>6</span>
-            <span className='sadface'>&#9785;</span>
-          </li>
-          <li className='analysis__item'>
-            Glucosa <span>68</span>
-            <span className='happyface'>&#9786;</span>
-          </li>
-          <li className='analysis__item'>
-            Calcio <span>90</span>
-            <span className='sadface'>&#9785;</span>
-          </li>
-        </ul>
       </section>
     </div>
   );
